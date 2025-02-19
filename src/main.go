3f1e463c676a8df7aa9a540a6abb67086721d62e
ShinYoungchan <zxc789zx@naver.com>
2025-02-19 14:59:52 +0900

    GET Method Example

--- src/main.go
+++ src/main.go
@@ -9,5 +9,16 @@ func main() {
 		return c.SendString("Hello, World!")
 	})
 
+	// GET 방식 예제
+	app.Get("/fiber", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, get fiber!")
+	})
+
+	// GET 요청: 동적 경로 (URL 파라미터)
+	app.Get("/user/:name", func(c *fiber.Ctx) error {
+		name := c.Params("name")
+		return c.SendString("Hello, " + name)
+	})
+
 	app.Listen(":8080")
 }
