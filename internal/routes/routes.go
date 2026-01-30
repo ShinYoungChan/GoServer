5bc1acd9f664b863f89ce81f8e9f9083dc2c2a2e
ShinYoungchan <zxc789zx@naver.com>
2026-01-30 15:56:45 +0900

    글 수정 및 삭제 기능 구현 및 테스트

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -19,7 +19,9 @@ func Register(r *gin.Engine) {
 		aritlcleRoutes.GET("/create", handlers.ShowArticleCreatePage)
 		// article 생성 이후 저장 post
 		aritlcleRoutes.POST("/create", handlers.PerformCreateArticle)
-		aritlcleRoutes.DELETE("/:article_id", handlers.DeleteArticle)
+		// 기존 r.DELETE를 r.POST로 변경
+		//aritlcleRoutes.DELETE("/:article_id", handlers.PerformDeleteArticle)
+		r.POST("/article/delete/:article_id", handlers.PerformDeleteArticle)
 		// 수정 페이지 보여주기 (기존 데이터가 채워진 폼)
 		aritlcleRoutes.GET("/edit/:article_id", handlers.ShowArticleEditPage)
 		// 수정된 데이터 처리하기
